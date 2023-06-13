2dbc0241f05a4810ee712524d302dd9846470e80
Victor Mao <victor@Victors-MacBook-Pro.local>
2023-06-12 22:13:53 -0700

    Hyperparameter Tuning

--- src/utils.py
+++ src/utils.py
@@ -1,25 +1,24 @@
+import os
+import sys
+
 import numpy as np 
 import pandas as pd
-import os
 import dill
-import sys 
-
 import pickle
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
 
-from src.exception import CustomException 
+from src.exception import CustomException
 
 def save_object(file_path, obj):
-    try: 
+    try:
         dir_path = os.path.dirname(file_path)
 
         os.makedirs(dir_path, exist_ok=True)
 
-        with open(file_path, 'wb') as file_obj:
-            dill.dump(obj, file_obj)
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
 
-    
     except Exception as e:
         raise CustomException(e, sys)
     
